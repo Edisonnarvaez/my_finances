04b74122b8e8143482bec43325f51948984a2f27
EdisonNarvaez01 <stiven.98020@gmail.com>
2024-05-09 00:08:27 -0500

    configuraciones

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:my_finances/modules/authentication/screens/onboarding/onboarding.dart';
+import 'package:my_finances/utils/theme/theme.dart';
+
+//import 'package:app_mobile/utils/theme/theme.dart';
+//import 'package:app_mobile/modules/authentication/screens/onboarding/onboarding.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const OnBoardingScreen(),
+    );
+  }
+}
